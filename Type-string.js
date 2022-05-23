10e3eb5dd2d0aa06839b02e691d53b3c54281934
Andre Luiz <andreluizmorettoa@gmail.com>
2022-05-23 12:49:13 -0300

    exemplos da aula 1

---
+++ Type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Ola Mundo!";
+const texto2 = 'Ola Mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+// da pra usar "" dentro de '' ou contrario 
+//exemplo
+const texto3 = "ele falou 'Ola'"
+
+// concatenacao (+)
+
+const citacao = "Meu nome é ";
+const meuNome = "Andre";
+
+console.log(citacao + meuNome)
---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+//true = verdadeiro 
+//false = falso
+// === = comparacao 
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+//console.log(primeiroNumero === segundoNumero)
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
