4850334dc69dc915018174657ac821b766fbcfa5
André Luiz <andreluizmorettoa@gmail.com>
2022-11-01 16:25:11 -0300

    template

---
+++ template-strings.js
@@ -0,0 +1,14 @@
+//concatenação
+
+const nome = "andre";
+const idade = 2022 - 2000;
+const cidadeNascimento = "Criciuma"
+//const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + 
+cidadeNascimento;
+
+//template
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentacao)
+
+
