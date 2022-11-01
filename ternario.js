d914321dcc364cca765defee1aa429d48ef84e84
André Luiz <andreluizmorettoa@gmail.com>
2022-11-01 14:01:49 -0300

    ternario

---
+++ ternario.js
@@ -0,0 +1,15 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// if (idadeCliente >= idadeMinima){
+//     console.log("cerveja")
+// } else{
+//     console.log("suco")
+// }
+//return suco
+
+//ternario
+            //condição                     //true    //false
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco")
+//return suco
+//boa pratica para pequenas condições
